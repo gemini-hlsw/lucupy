9da4ced9d0d64493d71957f4a608e4892a102343
Sergio <sergio.troncoso@noirlab.edu>
2022-08-13 00:15:08 -0400

    Fix import paths and test suites

---
+++ lucupy/observatory/gemini/__init__.py
@@ -0,0 +1,2 @@
+from .geminiproperties import *
+from .geminiobservation import *
---
+++ lucupy/output/__init__.py
@@ -0,0 +1,31 @@
+
+from minimodel import Group, Observation, Program
+from typing import NoReturn
+
+
+def print_program(program: Program) -> NoReturn:
+    """
+    Print the high-level information about a program in human semi-readable format
+    to give an idea as to its structure.
+    """
+    print(f'Program: {program.id}')
+
+    def sep(depth: int) -> str:
+        return '----- ' * depth
+
+    def print_observation(depth: int, obs: Observation) -> NoReturn:
+        print(f'{sep(depth)} Observation: {obs.id}')
+        for atom in obs.sequence:
+            print(f'{sep(depth + 1)} {atom}')
+
+    def print_group(depth: int, group: Group) -> NoReturn:
+        # Is this a subgroup or an observation?
+        if isinstance(group.children, Observation):
+            print_observation(depth, group.children)
+        elif isinstance(group.children, list):
+            print(f'{sep(depth)} Group: {group.id}')
+            for child in group.children:
+                print_group(depth + 1, child)
+
+    # Print the group and atom information.
+    print_group(1, program.root_group)
