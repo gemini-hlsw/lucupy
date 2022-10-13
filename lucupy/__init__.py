7f91848bed14e17c32c140b54a319af44f97aa0d
Sergio <sergio.troncoso@noirlab.edu>
2022-10-13 12:20:55 -0300

    SCHED-219: Modified Readme

--- lucupy/__init__.py
+++ lucupy/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
 # For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
-""" #Lucupy
+""" # Lucupy
 
 This package contains data structures and functions to support all the microservices that make up the Schedule app 
 for the Gemini Program Plataform (and other auxiliary services such as Env and Resource)
@@ -16,4 +16,4 @@ for the Gemini Program Plataform (and other auxiliary services such as Env and R
 - Decorators: A collection of decorators (Currently empty)
 
 """
-__version__ = '0.1.2'
+__version__ = '0.1.3'
