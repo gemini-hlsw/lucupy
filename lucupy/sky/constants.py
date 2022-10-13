85ace125ed225bb521650159d0220e9cd5990800
Sebastian Raaphorst <sraaphorst@gmail.com>
2022-10-12 18:28:18 -1000

    SCHED-230: Added note about license change.

--- lucupy/sky/constants.py
+++ lucupy/sky/constants.py
@@ -4,11 +4,8 @@
 #
 # utility and miscellaneous time and the sky routines built mostly on astropy.
 #
-# Copyright John Thorstensen, 2018; offered under the GNU Public License 3.0.
-#
-# Vectorized by Bryan Miller, Gemini Observatory.
-# Refactored and clarified by Sergio Troncoso, Gemini Observatory.
-# Modified by Sebastian Raaphorst, Gemini Observatory, to remove all deviations from Python 3.x PEP8 style guide.
+# Copyright John Thorstensen, 2018, who graciously has allowed Gemini to use this code under the BSD-3 Clause license.
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
 from typing import Final
 
