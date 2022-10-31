998197b4a8ee885fbf9b8ca5043acaf0fdf7604d
Sebastian Raaphorst <sraaphorst@gmail.com>
2022-10-31 10:42:48 -1000

    General cleanup, adding missing headers.

--- lucupy/observatory/__init__.py
+++ lucupy/observatory/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 """Observatory is used to change behavior on some of the structures and components
    in the Scheduler service and others.
 
