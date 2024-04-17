15c1a8a8dadfe89b600577c095d5173daddf9118
Bryan Miller <millerwbryan@gmail.com>
2024-04-17 11:48:14 -0400

    Added MinMax class to types

--- lucupy/types/__init__.py
+++ lucupy/types/__init__.py
@@ -3,6 +3,7 @@
 
 from datetime import timedelta
 from typing import Callable, Final, Generic, List, TypeAlias, TypeVar
+from enum import auto, Enum
 
 import numpy.typing as npt
 from astropy.time import Time
@@ -28,3 +29,15 @@ class Instantiable(Generic[T]):
 
     def __call__(self) -> T:
         return self.func()
+
+
+class MinMax(Enum):
+    '''
+    Enum typically to be used as dictionary keys when we have a dictionary
+    that has both a min value and a max value key: that way we do not have
+    to repeat the magic strings 'min' and 'max' in the code.
+
+    Furthermore, it will always be the case that MIN < MAX, in case that is needed.
+    '''
+    MIN = auto()
+    MAX = auto()
