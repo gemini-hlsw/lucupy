27ab946fad8ad2d6652780c2ef6dc9762ac8ce75
Bryan Miller <millerwbryan@gmail.com>
2024-07-25 21:53:45 -0400

    Reintroduce ObservationID.program_id()

--- lucupy/minimodel/ids.py
+++ lucupy/minimodel/ids.py
@@ -55,42 +55,23 @@ class UniqueGroupID(ID):
     ...
 
 
-def obs_to_program_id(obs_id: str) -> ProgramID:
-    """Return program ID string from OCS observation ID or GPP observation reference label string"""
-    return ProgramID(obs_id[0:obs_id.rfind('-')])
-
 @final
 @dataclass(frozen=True)
 class ObservationID(ID):
-    """Observation ID. For general GPP support we store the program ID rather than generate it from
-    the obsid string
+    """Observation ID
     """
-    program_id: Optional[ProgramID] = None
+    # program_id: Optional[ProgramID] = None
 
     @property
     def to_unique_group_id(self) -> UniqueGroupID:
         return UniqueGroupID(self.id)
 
-    def __hash__(self):
-        """
-        We only want to hash the ID. Obsids with identical IDs should be considered identical.
-        """
-        return hash(self.id)
+    def program_id(self) -> ProgramID:
+        """Return program ID string from observation ID string"""
+        return ProgramID(self.id[0:self.id.rfind('-')])
 
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(id={self.id})'
-
-    def __le__(self, other) -> bool:
-        return self.id < other.id
-
-    def __lt__(self, other) -> bool:
-        return self.id <= other.id
-
-    def __ge__(self, other) -> bool:
-        return self.id > other.id
-
-    def __gt__(self, other) -> bool:
-        return self.id >= other.id
-
-    def __eq__(self, other) -> bool:
-        return type(self) is type(other) and self.id == other.id
+    # def __hash__(self):
+    #     """
+    #     We only want to hash the ID. Obsids with identical IDs should be considered identical.
+    #     """
+    #     return hash(self.id)
